f4178e4ad85506563ebe299fa7cc663baf3843cb
Santosh <santosh.pandeyy@gmail.com>
2025-03-24 18:02:09 +0530

    further-changes

--- assignment-33/app/main.py
+++ assignment-33/app/main.py
@@ -4,17 +4,6 @@ from client.google_client import get_response
 app = FastAPI()
 
 
-#@app.get("/message")
-#def read_root():
-    #client = genai.Client(api_key="AIzaSyAhhxxYBbP3SEIhQj-TF_dMNKlPeiG_kHk")
-    #response = client.models.generate_content(
-        #model="gemini-2.0-flash",
-        #contents="Calculate the sum of exponentials of first 3 Fibonacci numbers",
-    #)
-    #print(response.text)
-
-    #return response.text
-
 @app.get("/message2/")
 def read_root():
     return get_response()    
