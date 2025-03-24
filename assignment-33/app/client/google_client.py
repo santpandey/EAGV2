154f31f39bdd337c3a45c731f3091f6f1571f0ae
Santosh <santosh.pandeyy@gmail.com>
2025-03-24 18:00:20 +0530

    initial-change-assignment

--- assignment-33/app/client/google_client.py
+++ assignment-33/app/client/google_client.py
@@ -1,9 +1,9 @@
 from google import genai
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
-print(api_key)
 client = genai.Client(api_key=api_key)
 
 
