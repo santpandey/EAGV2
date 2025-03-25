02934a04107a162486de40118e300c985e75e1b1
Santosh <santosh.pandeyy@gmail.com>
2025-03-25 18:14:49 +0530

    fixed-changes

--- assignment-33/app/client/google_client.py
+++ assignment-33/app/client/google_client.py
@@ -8,8 +8,12 @@ client = genai.Client(api_key=api_key)
 
 
 def get_response( message: str):
-    if not message:
-        message = "Calculate the sum of exponentials of first 3 Fibonacci numbers"
+    print("Message is ",message)
+    default_message = "Calculate the sum of exponentials of first 3 Fibonacci numbers"
+    if len(message.strip()) == 0:
+        print("Message is empty")
+        message = default_message
+    print("Message is ",message)
     response = client.models.generate_content(
         model="gemini-2.0-flash",
         contents=message,
--- assignment-33/app/main.py
+++ assignment-33/app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from client.google_client import get_response
-#from google import genai
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
+origins = ["*","http://localhost:8000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/get_llm_response/{message}")
 def read_root(message: str):
